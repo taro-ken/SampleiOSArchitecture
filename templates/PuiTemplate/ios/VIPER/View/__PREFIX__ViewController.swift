d338c032dffdf97d375b22c3734463e28573aa08
taro-ken <67847164+taro-ken@users.noreply.github.com>
2021-06-22 18:49:07 +0900

    Add files via upload

---
+++ templates/PuiTemplate/ios/VIPER/View/__PREFIX__ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  __PREFIX__View.swift
+//  __TARGET__
+//
+//  Created by __USERNAME__ on __YEAR__/__MONTH__/__DAY__.
+//
+
+import UIKit
+
+protocol __PREFIX__View: AnyObject {}
+
+final class __PREFIX__ViewController: UIViewController {
+  static func makeFromStoryboard() -> __PREFIX__ViewController {
+    let vc = UIStoryboard.load__PREFIX__()
+    return vc
+  }
+
+  private var presenter: __PREFIX__Presentation!
+  func inject(presenter: __PREFIX__Presentation) {
+    self.presenter = presenter
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    presenter.viewDidLoad()
+  }
+}
+
+extension __PREFIX__ViewController: __PREFIX__View {}
